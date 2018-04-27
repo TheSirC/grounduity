30e2e49237e5265aaa83e24581d336447fa96d1e
King Claudy <clocloranely@hotmail.fr>
2018-04-27 13:32:39 -0400

    Modificaions visuelles et ajout des variables pour tiers

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,18 @@ Container buildButtonColumn(IconData icon, String label, Color color) {
       children: [
         new Container(
           margin: EdgeInsets.only(top: 20.0),
-          child: new Icon(icon, color: color),
+          child: new Icon(
+            icon,
+            color: color,
+            size: 40.0,
+          ),
         ),
         new Container(
           margin: const EdgeInsets.only(top: 7.0),
           child: new Text(
             label + '%',
             style: new TextStyle(
-              fontSize: 12.0,
+              fontSize: 20.0,
               fontWeight: FontWeight.w400,
               color: color,
             ),
@@ -29,9 +33,15 @@ Container buildButtonColumn(IconData icon, String label, Color color) {
 }
 
 class MyApp extends StatelessWidget {
+
   double billAmount = 0.0;
   double tipPercentage = 0.0;
+  int tipTier1 = 10;
+  int tipTier2 = 15;
+  int tipTier3 = 20;
   String monetarySymbol = "\$";
+
+
   final appName = 'Grounduity';
   // This widget is the root of your application.
   @override
@@ -46,48 +56,61 @@ class MyApp extends StatelessWidget {
           ),
           body: new Align(
             alignment: Alignment(0.5, 0.5),
-            child: new Column(children: <Widget>[
-              new Container(
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: new TextField(
-                    keyboardType: TextInputType.number,
-                    autofocus: true,
-                    decoration: new InputDecoration(
-                      prefixText: monetarySymbol,
-                        labelText: "Bill before gratuity", hintText: '10'),
-                    onChanged: (String value) {
-                      try {
-                        billAmount = double.parse(value);
-                      } catch (exception) {
-                        billAmount = 0.0;
-                      }
-                    },
-                  )),
-              new Row(
-                children: <Widget>[
-                  buildButtonColumn(
-                      Icons.attach_money, '10', Color(0xFF388E3C)),
-                  buildButtonColumn(
-                      Icons.attach_money, '15', Color(0xFF388E3C)),
-                  buildButtonColumn(
-                      Icons.attach_money, '20', Color(0xFF388E3C)),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-              ),
-              new Row(
-                children: <Widget>[
-                  new Container(
-                      margin: EdgeInsets.all(40.0),
-                      child: new Text("Calculated gratuity")),
-                  new Container(
-                      margin: EdgeInsets.all(40.0),
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                      child: new Text("Total"))
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              )
-            ],mainAxisAlignment: MainAxisAlignment.spaceEvenly,),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                    margin: EdgeInsets.all(20.0),
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      autofocus: true,
+                      decoration: new InputDecoration(
+                        prefixText: monetarySymbol,
+                        labelText: "Bill before gratuity",
+                        labelStyle: TextStyle(fontSize: 25.0),
+                        hintText: '10',
+                      ),
+                      onChanged: (String value) {
+                        try {
+                          billAmount = double.parse(value);
+                        } catch (exception) {
+                          billAmount = 0.0;
+                        }
+                      },
+                    )),
+                new Row(
+                  children: <Widget>[
+                    buildButtonColumn(
+                        Icons.attach_money, tipTier1.toString(), Color(0xFF388E3C)),
+                    buildButtonColumn(
+                        Icons.attach_money, tipTier2.toString(), Color(0xFF388E3C)),
+                    buildButtonColumn(
+                        Icons.attach_money, tipTier3.toString(), Color(0xFF388E3C)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Container(
+                        margin: EdgeInsets.all(40.0),
+                        child: new Text(
+                          "Gratuity",
+                          maxLines: 1,
+                          textScaleFactor: 1.75,
+                        )),
+                    new Container(
+                        margin: EdgeInsets.all(40.0),
+                        child: new Text(
+                          "Total",
+                          maxLines: 1,
+                          textScaleFactor: 1.75,
+                        ))
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
           ),
         ));
   }
